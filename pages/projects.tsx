440e21717d2e447a7be91f9ffcf05b08e1bc2568
Lokesh3294 <llokesh26204@gmail.com>
2025-06-09 10:26:06 +0530

    Title error was fixed

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,6 +1,6 @@
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
-import { title } from 'process';
+// import { title } from 'process';
 
 const projects = [
   {
